8fd1fc65fe46ff1b1932b644bc390deb39e7bac2
mwiguna <mwiguna33@gmail.com>
2022-07-07 11:11:45 +0700

    init reactportfolio

---
+++ reactportfolio/src/component/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Whenever you're, i only want you to know that <br/> I Always Love You</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
