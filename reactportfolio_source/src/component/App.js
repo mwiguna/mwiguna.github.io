03ff0771da479182e372b7a9f4bd911e87262ca0
mwiguna <mwiguna33@gmail.com>
2022-07-07 14:01:28 +0700

    change route

--- reactportfolio_source/src/component/App.js
+++ reactportfolio_source/src/component/App.js
@@ -5,17 +5,19 @@ import RiwayatPendidikan from './RiwayatPendidikan';
 import '../App.css';
 
 function App() {
+  const base_url = "/reactportfolio_source/build";
+
   return (
     <div className="App">
-      <Link to="/" className="pr-1">Home</Link>
-      <Link to="/kontak" className="px-1">Kontak</Link>
-      <Link to="/riwayat_pendidikan/1" className="px-1">RiwayatPendidikan 1</Link>
-      <Link to="/riwayat_pendidikan/2" className="px-1">RiwayatPendidikan 2</Link>
+      <Link to={base_url + "/"} className="pr-1">Home</Link>
+      <Link to={base_url + "/kontak"} className="px-1">Kontak</Link>
+      <Link to={base_url + "/riwayat_pendidikan/1"} className="px-1">RiwayatPendidikan 1</Link>
+      <Link to={base_url + "/riwayat_pendidikan/2"} className="px-1">RiwayatPendidikan 2</Link>
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/kontak" element={<Kontak />} />
-        <Route path="/riwayat_pendidikan/:id" element={<RiwayatPendidikan />} />
+        <Route path={base_url + "/"} element={<HomePage />} />
+        <Route path={base_url + "/kontak"} element={<Kontak />} />
+        <Route path={base_url + "/riwayat_pendidikan/:id"} element={<RiwayatPendidikan />} />
       </Routes>
 
     </div>
